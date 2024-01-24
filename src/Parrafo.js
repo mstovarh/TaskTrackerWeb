9d7501fc3760e5b3e6ed2de364185f13509c20c0
Maria Stephanie Tovar <ingmariatovar@gmail.com>
2024-01-24 17:41:21 -0500

    Updates

--- src/Parrafo.js
+++
@@ -1,9 +0,0 @@
-import './Parrafo.css';
-
-function Parrafo({total, completed}) {
-    return(
-      <p className='msjp'>Hola. Has completado {completed} de {total}</p>
-    );
-}
-  
-export { Parrafo };
